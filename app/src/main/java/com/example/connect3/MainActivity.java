13178ab3533b4362d10033b3a7444d6057762b59
Alek Jacoby <alekjacoby@gmail.com>
2019-11-03 21:01:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.connect3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0: yellow, 1: red, 2: empty
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
+    int activePlayer = 0;
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+        Log.i("Tag", counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedCounter] = activePlayer;
+        counter.setTranslationY(-1500);
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.green);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
+        counter.animate().translationYBy(1500).setDuration(300);
+
+        for (int[] winningPosition : winningPositions) {
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] !=2) {
+                // Someone has won!
+                Toast.makeText(this, "Someone has won!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
